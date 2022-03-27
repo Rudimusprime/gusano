5c54a8cf73baad6a8fa18de2d0863758d4158b0d
Daniel Banasiewicz <d.banan1337@gmail.com>
2022-03-27 20:28:58 +0200

    add hamburger logo for pc

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,32 +2,45 @@ import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
 import navBarLogo from "../../images/nav-bar logo.svg";
 import './index.css';
+import './hamburger.scss';
 
+const menu = document.querySelector('.hamburger');
+const handleToggleClass = () => {
+    menu.classList.toggle('hamburger--active');
+}
 const Navigation = () => {
     return (
+
         <Router>
             <header className="navbar">
-                    <a href="#">
-                        <img alt=""
-                             className="navbar-logo"
-                             src={navBarLogo}/>
-                    </a>
-                    <nav className="nav-links">
-                        <ul>
-                            <li>
-                                <a href="#about">o mnie</a>
-                            </li>
-                            <li>
-                                <a href="#graphics">grafika</a>
-                            </li>
-                            <li>
-                                <a href="#sketch">rysunek</a>
-                            </li>
-                            <li>
-                                <a href="#kontakt">kontakt</a>
-                            </li>
-                        </ul>
-                    </nav>
+                <a href="#">
+                    <img alt=""
+                         className="navbar-logo"
+                         src={navBarLogo}/>
+                </a>
+                <button className="hamburger"
+                        aria-label="Menu"
+                        onClick={handleToggleClass}>
+                <span className="hamburger__container" tabIndex="-1">
+                  <span className="hamburger__bars"></span>
+                </span>
+                </button>
+                <nav className="nav-links">
+                    <ul>
+                        <li>
+                            <a href="#about">o mnie</a>
+                        </li>
+                        <li>
+                            <a href="#graphics">grafika</a>
+                        </li>
+                        <li>
+                            <a href="#sketch">rysunek</a>
+                        </li>
+                        <li>
+                            <a href="#kontakt">kontakt</a>
+                        </li>
+                    </ul>
+                </nav>
             </header>
         </Router>
     );
